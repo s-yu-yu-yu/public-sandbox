21f9228b49c2c58fc650619766b82688eec5d8cc
s-yu-yu-yu <carefreektkt@gmail.com>
2024-04-11 00:52:18 +0900

    feat: vketcloudの埋め込み

---
+++ pages/vket-embed.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+useHead({
+  title: "Vket Embed",
+  meta: [
+    {
+      name: "description",
+      content: "Vket Embed",
+    },
+    {
+      property: "twitter:player",
+      content:
+        "https://myvketofficial.cloud.vket.com/worlds/24Summer_iframe/main.html?worldid=24Summer_iframe",
+    },
+    {
+      property: "twitter:player:width",
+      content: "640",
+    },
+    {
+      property: "twitter:player:height",
+      content: "480",
+    },
+  ],
+});
+</script>
+
+<template>
+  <div>
+    <iframe
+      src="https://myvketofficial.cloud.vket.com/worlds/24Summer_iframe/main.html?worldid=24Summer_iframe"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      class="iframe"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+.iframe {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
